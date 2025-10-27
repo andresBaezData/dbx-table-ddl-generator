133d8b744d1345922006c69cb00f9d9035af959b
Andres Baez <andres.baez@lovelytics.com>
2025-10-27 15:31:24 -0500

    Objects details filtered and filter details created

--- extraction/extract_info.py
+++ extraction/extract_info.py
@@ -8,11 +8,19 @@ class Extraction():
         self.excel_file = pd.ExcelFile(self.input_path)
         self.excel_info: Dict[str, pd.DataFrame] = {}
         self._read_file()
-        print(self.excel_info)
     
     def _read_file(self):
         """
+        Read from the same excel file, the tabs: Table Details, Joins and Object Details and save each one in excel_info
         """
         for col in self.file_columns:
             df = pd.read_excel(self.excel_file, sheet_name= col, engine="openpyxl", header= 1)
             self.excel_info[col] = df
+    
+    def get_info(self):
+        """
+        """
+        self._read_file()
+        objects_details = self.excel_info['Object Details']
+        objects_details_filtered = objects_details[(objects_details['Obj Select'].notnull()) & (objects_details['Obj Where'].isnull())].copy()
+        filter_details = objects_details[(objects_details['Obj Where'].notna())].copy()
